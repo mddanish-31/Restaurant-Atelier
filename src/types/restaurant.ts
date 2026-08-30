af33c444a634f6f72f80175ca41a485b5269130c
mddanish-31 <mddanishraza904@gmail.com>
2026-08-30 23:39:58 +0530

    Create restaurant.ts

---
+++ src/types/restaurant.ts
@@ -0,0 +1,11 @@
+export interface Restaurant {
+  id: string;
+  name: string;
+  cuisine: string;
+  image: string;
+  imageAlt: string;
+  rating: number;
+  location: string;
+  priceRange: string;
+  note: string;
+}
