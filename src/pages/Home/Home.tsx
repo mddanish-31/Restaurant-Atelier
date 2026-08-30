0cd8734020fb3aab6880a39f42b9d7adc3fbec04
mddanish-31 <mddanishraza904@gmail.com>
2026-08-30 23:39:49 +0530

    Update Home.tsx

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,21 @@
 import { Hero } from '@/pages/Home/Hero';
 import { FeatureStrip } from '@/components/home/FeatureStrip';
+import { ExploreCategoriesSection } from '@/components/home/ExploreCategoriesSection';
 import { FeaturedFoodSection } from '@/components/food/FeaturedFoodSection';
+import { RestaurantDiscoverySection } from '@/components/restaurant/RestaurantDiscoverySection';
+import { AboutSection } from '@/components/home/AboutSection';
+import { FinalCtaSection } from '@/components/home/FinalCtaSection';
 
 export default function Home() {
   return (
     <>
       <Hero />
       <FeatureStrip />
+      <ExploreCategoriesSection />
       <FeaturedFoodSection />
+      <RestaurantDiscoverySection />
+      <AboutSection />
+      <FinalCtaSection />
     </>
   );
 }
