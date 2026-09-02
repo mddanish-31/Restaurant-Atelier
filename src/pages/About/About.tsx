5b3c711a49f60e4659bd55f8a90823f8a6fc895b
mddanish-31 <mddanishraza904@gmail.com>
2026-09-02 14:49:49 +0530

    Update About.tsx

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,5 +1,23 @@
-import { PlaceholderPage } from '@/components/common/PlaceholderPage';
+import { AboutHero } from './AboutHero';
+import { Philosophy } from './Philosophy';
+import { TheWayWeCook } from './TheWayWeCook';
+import { TheExperience } from './TheExperience';
+import { StoryMoment } from './StoryMoment';
+import { Principles } from './Principles';
+import { EventsSection } from '../Menu/EventsSection';
+import { AboutCta } from './AboutCta';
 
 export default function About() {
-  return <PlaceholderPage title="About" />;
+  return (
+    <main>
+      <AboutHero />
+      <Philosophy />
+      <TheWayWeCook />
+      <TheExperience />
+      <StoryMoment />
+      <Principles />
+      <EventsSection />
+      <AboutCta />
+    </main>
+  );
 }
